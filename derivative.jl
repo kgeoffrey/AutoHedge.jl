d7676b1d6b8a966da6c2e415597726c4d9a44378
Geoffrey Kasenbacher <gkasenbacher@gmail.com>
2019-11-29 15:53:25 +0100

    done

--- derivative.jl
+++ derivative.jl
@@ -1,13 +1,16 @@
 ### simple Automatic Differentiation with Dual Numbers
-using Distributions
+using Distributions, LinearAlgebra
 
 struct Dual <: Number
     f::Real
-    g::Real
+    g::Array
 end
 
-### Differentiation rules ###
+### Differentiation rules via overloading ###
+
+import Base: +,/,*,-,^,adjoint, convert, promote_rule
 import Base: +,/,*,-,^, convert, promote_rule
+
 +(x::Dual, y::Dual) = Dual(x.f + y.f, x.g + y.g)
 +(x::Dual, y::Real) = Dual(x.f + y, x.g)
 +(y::Real, x::Dual) = Dual(x.f + y, x.g)
@@ -25,30 +28,45 @@ import Base: +,/,*,-,^, convert, promote_rule
 Base.exp(x::Dual) = Dual(exp(x.f), x.g * exp(x.f))
 Base.sqrt(x::Dual) = Dual(sqrt(x.f), x.g / (2 * sqrt(x.f)))
 Base.log(x::Dual) = Dual(log(x.f), x.g/x.f)
-Distributions.cdf(d, x::Dual) = Dual(cdf(d, x.f), pdf(d, x.f) * x.g)
 
-### convert, promote ###
+Distributions.cdf(d, x::Dual) = Dual(cdf(d, x.f), pdf(d, x.f) * x.g)
+Base.adjoint(x::Dual) = Dual(adjoint(x.f), adjoint(x.g))
+LinearAlgebra.dot(x::Dual, y::Dual) = Dual(dot(x.f,y.f), x.f * y.g + y.f * x.g)
+Base.zero(x::Dual) = Dual(zero(x.f), zero(x.g))
+
+### conversion, promotion rules ###
 convert(::Type{Dual}, x::Real) = Dual(x, one(x))
+convert(::Type{Dual}, x::AbstractArray) = DualArray(x)
+convert(::Type{Array}, x::Real) = [x]
 promote_rule(::Type{Dual}, ::Type{<:Number}) = Dual
 
 
 ### derivative ###
-function derivative(f, x)
-    return f(Dual(x,1)).g
+function gradient(f, x)
+    x = convert(Dual, x)
+    return getfield.(f(x), :g)
 end
 
+new = rand(100)
 
-struct Darray
-    f::Array
-    g::Array
+sigmoid(x) = x' * x
+derivative(sigmoid, new)
+
+
+
+Dual(1,1)
+getfield(Dual(1,1), :g)
+
+function DualArray(x)
+    l = length(x)
+    eye = I(l)
+    collect = []
+    for i in 1:l
+        push!(collect, Dual(x[i], view(eye,i,:,)))
+    end
+    return collect
 end
-import Base: +,/,*,-,^, convert, promote_rule
-convert(::Type{Darray}, x::AbstractArray) = Darray(x, ones(length(x)))
 
-+(x::Darray, y::Darray) = Darray(x.f .+ y.f, x.g .+ y.g)
--(x::Darray, y::Darray) = Darray(x.f .- y.f, x.g .- y.g)
-*(x::Darray, y::Darray) = Darray(x.f*y.f, x.f*y.g + y.f*x.g)
+t = DualArray(new)
 
-r = convert(Darray, t)
-
-r'*r
+@time gradient(sigmoid, new)
