f38bd1c061001ef938d9630f6b3ff0f09d7f38ad
Geoffrey Kasebacher <gkasenbacher@gmail.com>
2023-03-12 16:04:03 +0100

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,6 @@
-include("../src/AutoHedge.jl")
-using .AutoHedge
-#using AutoHedge
+using AutoHedge
+using Test
 
-
-f1 = CallOption(100., 130., 5., 0.01, 0.2, 0.)
-f2 = CallOption(100., 140., 5., 0.01, 0.2, 0.)
-f3 = CallOption(100., 115., 5., 0.01, 0.2, 0.)
-f4 = PutOption(100., 120., 5., 0.01, 0.2, 0.)
-stock = UnderlyingStock() #(100., 0, 0)
-p = Portfolio(f1, 10, [stock, f2, f4], ["delta", "theta", "vanna"])
-
-stock_price = randomwalk(100, 500, 1)
-rebalancing_frequency = 5
-borrowing, volumes, tracking_errors = backtest(stock_price, rebalancing_frequency, p)
-
-
-# plots
-#s1 = plot(stock_price, xlabel="Time", ylabel = "Spot Price")
-#s2 = plot(tracking_errors, xlabel="Time", ylabel = "Tracking Error (Value of Portfolio)")
-#s3 = plot(volumes, labels=permutedims(string.(typeof.(p.hedging_instruments))), xlabel="Time", ylabel = "Volume")
-#s4 = plot(borrowing, xlabel="Time", ylabel = "Cash Borrowing")
+@testset "AutoHedge.jl" begin
+    @test length(AutoHedge.randomwalk(100, 500, 1)) == 500
+end
